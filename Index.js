f0af4b034a07647076ead4932f9098c33cda46c1
Esteban <estebanmarquez.c@gmail.com>
2022-09-14 22:45:51 -0300

    Version final

--- Index.js
+++ Index.js
@@ -1,67 +1,91 @@
-document.querySelectorAll(".reservados").forEach((evento) => {
-  evento.addEventListener("click", (e) => {
-    console.log(e.target.id);
-  });
-});
-const descripcionesEventos = new Map();
-descripcionesEventos.set("afrojack", "Afrojack en OVO Punta del Este 5/11/2022");
-descripcionesEventos.set("shm", "Swedish House Mafia Estadio Centenario 15/10/2022");
-descripcionesEventos.set ("claptone", " Corona Sunsets de Punta del Este 31/12/2022");
 
-document.querySelectorAll(".reservados").forEach((item) => {
-  item.addEventListener("click", (event) => {
-    const idEvento = event.target.id;
-    const descripcionEvento = descripcionesEventos.get(idEvento);
-
-    let yaReservadas = localStorage.getItem(idEvento)
-      ? localStorage.getItem(idEvento)
-      : 0;
-    localStorage.setItem(idEvento, ++yaReservadas);
-
-    Swal.fire(
-      "Felicitaciones!",
-      `Reservaste entradas para ${descripcionEvento}`,
-      "success"
-    );
-  });
-});
-
-document.querySelectorAll(".ampliar").forEach((eventoPasado) => {
-  eventoPasado.addEventListener("click", (e) => {
-    console.log(e.target.id);
-  });
-});
-
-const descripcionesEventosPasados = new Map();
-descripcionesEventosPasados.set("resistance", {
-  img:"https://uruguay.resistancemusic.com/wp-content/uploads/sites/18/2019/08/uruguay-og.jpg",
-  descripcion: "Resistance 2018 Velodromo"
-});
-descripcionesEventosPasados.set("galantis",{
-  img:"https://edmidentity.com/wp-content/uploads/2017/11/59c8be4b78d19946401803.png.jpeg",
-  descripcion: "Galantis Velodromo"});
-
-descripcionesEventosPasados.set("carlcox",{
-  img: "https://sc2.elpais.com.uy/files/article_main/uploads/2018/10/11/5bbf6579f3f17.jpeg",
-  descripcion:"Carlcox Resistance 2019"});
-
-document.querySelectorAll(".ampliar").forEach(item => {
-
-  item.addEventListener("click", (event) => {
-    const idEventoPasado = event.target.id;
-    const infoEvento =
-      descripcionesEventosPasados.get(idEventoPasado);
-
+const cargarEventosActivos = () => {
+  fetch('https://6311528d826b98071a556980.mockapi.io/conciertos')
+    .then((response) => response.json())
+    .then((eventos) => {
+       mostrarEventosActivos(eventos)
+      reservarEventosActivos(eventos);
+    });
+  }
   
-    Swal.fire({
-      title: idEventoPasado.charAt(0).toUpperCase() + idEventoPasado.slice(1, idEventoPasado.length),
-      text: infoEvento.descripcion,
-      imageUrl: infoEvento.img,
-      imageWidth: 400,
-      imageHeight: 200,
-      imageAlt: "Custom image",
+  const mostrarEventosActivos = (eventos) => {
+    let eventosActivos = document.getElementById("eventos-activos");
+
+    eventos.forEach(evento => {
+      eventosActivos.innerHTML +=
+        `
+      <div class="card">
+        <img
+          src="${evento.img}"
+          alt="${evento.nombre}"
+        />
+        <h4>${evento.nombre}</h4>
+        <p class="p">
+          ${evento.descripcion}
+        </p>
+        <a class="reservados" id="${evento.id}" href="#">Reservar</a>
+      </div>
+  `
+    })
+
+  }
+
+  const reservarEventosActivos = (eventos) => {
+
+    eventos.forEach(evento => {
+      const elemento =document.getElementById(evento.id)
+      elemento.addEventListener("click", _ => {
+        let yaReservadas = localStorage.getItem(evento.id)
+          ? localStorage.getItem(evento.id)
+          : 0;
+        localStorage.setItem(evento.id, ++yaReservadas);
+
+        Swal.fire(
+          "Felicitaciones!",
+          `Reservaste entradas para ${evento.nombre}`,
+          "success"
+        );
+      });
+    })
+  }
+
+  const mostrarDescripcionesEventosPasados = () => {
+    const descripcionesEventosPasados = new Map();
+    descripcionesEventosPasados.set("resistance", {
+      img: "https://uruguay.resistancemusic.com/wp-content/uploads/sites/18/2019/08/uruguay-og.jpg",
+      descripcion: "Resistance 2018 Velodromo"
+    });
+    descripcionesEventosPasados.set("galantis", {
+      img: "https://edmidentity.com/wp-content/uploads/2017/11/59c8be4b78d19946401803.png.jpeg",
+      descripcion: "Galantis Velodromo"
     });
 
-  });
+    descripcionesEventosPasados.set("carlcox", {
+      img: "https://sc2.elpais.com.uy/files/article_main/uploads/2018/10/11/5bbf6579f3f17.jpeg",
+      descripcion: "Carlcox Resistance 2019"
+    });
 
-});
+    document.querySelectorAll(".ampliar").forEach(item => {
+
+      item.addEventListener("click", (event) => {
+        const idEventoPasado = event.target.id;
+        const infoEvento =
+          descripcionesEventosPasados.get(idEventoPasado);
+
+
+        Swal.fire({
+          title: idEventoPasado.charAt(0).toUpperCase() + idEventoPasado.slice(1, idEventoPasado.length),
+          text: infoEvento.descripcion,
+          imageUrl: infoEvento.img,
+          imageWidth: 400,
+          imageHeight: 200,
+          imageAlt: "Custom image",
+        });
+
+      });
+
+    });
+  }
+
+  cargarEventosActivos();
+  mostrarDescripcionesEventosPasados();
--- carrito.js
+++ carrito.js
@@ -4,25 +4,85 @@
 // calcular precio total
 // ir agregando a una variable total
 
-const precioEntradas = new Map();
-precioEntradas.set("afrojack", 200);
-precioEntradas.set("claptone", 400);
-precioEntradas.set("shm", 600);
-let total = 0;
-const container = document.getElementById("entradas");
 
-Object.keys(localStorage).forEach((key) => {
-  const subtotal = localStorage[key] * precioEntradas.get(key);
+const mostrarCompras = () => {
+  let total = 0;
+  const container = document.getElementById("entradas");
 
-  total += subtotal;
+  Object.keys(localStorage).forEach((key, i) => {
+    fetch('https://6311528d826b98071a556980.mockapi.io/conciertos/' + key) //obtener elemento desde la api por ID
+      .then((response) => response.json())
+      .then((data) => {
+        const cantidadEntradas = localStorage[key]
+        const subtotal = cantidadEntradas * data.precio;
+        total += subtotal;
 
-  container.innerHTML += `
+        container.innerHTML += mostrarDetalle(data.id, data.nombre, cantidadEntradas, data.precio)
+
+        //mostrar el total cuando es el ultimo elemento de la lista
+        if (i === localStorage.length - 1) {
+          document.getElementById("total").innerHTML = `Total:  $ ${total}`;
+        }
+
+      });
+  });
+}
+
+const mostrarDetalle = (id, nombre, cantidad, precioEntrada) => {
+  return `
 <div class="flex-container">
-<h2>${key} - ${localStorage[key]} entrada${localStorage[key] > 1 ? "s" : ""}
-  </h2>
-<h4>subtotal: $${localStorage[key] * precioEntradas.get(key)}</h4>
-</div>
-`;
-});
+  <div class="cantidad">
+  <h2>${nombre} </h2>
+  <div class="quantity buttons_added">
+    <input type="button" value="-" onclick="restarEntradas('${id}',${precioEntrada})" class="minus">
+    <input type="number" step="1" min="1" max="" name="quantity" value="${cantidad}" class="input-text qty text" size="4" pattern="" inputmode="" id="cantidadEntradas${id}">
+    <input type="button" value="+" onclick="sumarEntradas('${id}',${precioEntrada})"class="plus">
+  </div>
+  <h2> entrada${cantidad > 1 ? "s" : ""} </h2>
+  </div>
 
-document.getElementById("total").innerHTML += `${total}`;
+<span class="precio" id="precio${id}">$ ${cantidad * precioEntrada}</span>
+</div>
+ `;
+}
+
+const restarEntradas = (id, precioEntrada) => {
+  const input = document.getElementById("cantidadEntradas" + id)
+  if (input.value > 0) {
+    const cantidad = --input.value
+    input.value = cantidad
+
+    const subtotal = document.getElementById(`precio${id}`)
+   
+    subtotal.innerHTML = `$ ${cantidad * precioEntrada}`
+
+    localStorage.setItem(id, cantidad)
+    calcularTotal()
+
+  }
+}
+
+const sumarEntradas = (id, precioEntrada) => {
+  const input = document.getElementById("cantidadEntradas" + id)
+
+  const cantidad = ++input.value
+  input.value = cantidad
+
+  const subtotal = document.getElementById(`precio${id}`)
+  subtotal.innerHTML = `$ ${cantidad * precioEntrada}`
+  
+  localStorage.setItem(id, cantidad)
+  calcularTotal()
+}
+
+
+
+const calcularTotal = () => {
+  let total = 0;
+  document.querySelectorAll(".precio").forEach(precio => {
+    total += parseInt(precio.innerHTML.split("$")[1])
+  })
+  document.getElementById("total").innerHTML = `Total:  $ ${total}`;
+}
+
+mostrarCompras()
